5f3a812d85d938e44b5706fe73daa09d83a77598
DocenkoG <docn@mail.ru>
2017-09-19 14:54:12 +0300

    Download file and unzip price

---
+++ aris_pro.py
@@ -0,0 +1,46 @@
+# -*- coding: UTF-8 -*-
+import os
+import os.path
+import logging
+import logging.config
+import io
+import sys
+import configparser
+import time
+import aris_pro_converter
+import aris_pro_downloader
+import shutil
+
+global log
+global myname
+
+
+
+def make_loger():
+    global log
+    logging.config.fileConfig('logging.cfg')
+    log = logging.getLogger('logFile')
+
+
+
+def main( ):
+    global  myname
+    global  mydir
+   
+    make_loger()
+    log.info('------------  '+ myname +'  ------------')
+
+    if  aris_pro_downloader.download( myname ) :
+        aris_pro_converter.convert2csv( myname )
+        shutil.copy2( myname + '.csv', 'c://AV_PROM/prices/' + myname +'/'+ myname + '.csv')
+        shutil.copy2( 'python.log',    'c://AV_PROM/prices/' + myname +'/python.log')
+
+if __name__ == '__main__':
+    global  myname
+    global  mydir
+    myname   = os.path.basename(os.path.splitext(sys.argv[0])[0])
+    mydir    = os.path.dirname (sys.argv[0])
+    if ('' != mydir) : os.chdir(mydir)
+    main( )
+ 
+#os.system(r'c:\prices\_scripts\remove_tmp_profiles.cmd')
