cfb1e61242c8b2e520c763b78255a241e4235b98
DocenkoG <docn@mail.ru>
2018-04-02 14:49:24 +0300

    Поменял Price_tools под XLSX. requests, много файлов, в каждом страницы - группы и бренды. валюта по формату ячейки

--- aris_pro.cmd
+++ aris_pro.cmd
@@ -1,4 +1,4 @@
 chcp 1251
 
 "c:\Program Files\Python35\python.exe" aris_pro.py
-rem pause
+ pause
--- price_tools.py
+++ price_tools.py
@@ -1,10 +1,150 @@
 # -*- coding: UTF-8 -*-
 import xlrd                         # для .xls
+import openpyxl                     # Для .xlsx
 import re
+import configparser
+import os.path
+
+
+
+def config_read( cfgFName ):
+    cfg = configparser.ConfigParser(inline_comment_prefixes=('#'))
+    if  os.path.exists('private.cfg'):     
+        cfg.read('private.cfg', encoding='utf-8')
+    if  os.path.exists(cfgFName):     
+        cfg.read( cfgFName, encoding='utf-8')
+    else: 
+        log.debug('Нет файла конфигурации '+cfgFName)
+    return cfg
+
+
+
+def is_file_fresh(fileName, qty_days):
+    qty_seconds = qty_days *24*60*60 
+    if os.path.exists( fileName):
+        price_datetime = os.path.getmtime(fileName)
+    else:
+        log.error('Не найден файл  '+ fileName)
+        return False
+
+    if price_datetime+qty_seconds < time.time() :
+        file_age = round((time.time()-price_datetime)/24/60/60)
+        log.error('Файл "'+fileName+'" устарел!  Допустимый период '+ str(qty_days)+' дней, а ему ' + str(file_age) )
+        return False
+    else:
+        return True
+
+
+
+def openX(fileName ):
+    typeX = fileName[fileName.find('.')+1 :]
+    if typeX.lower() == 'xlsx':
+        book = openpyxl.load_workbook(filename = fileName, read_only=False, keep_vba=False, data_only=False) # xlsx
+    else:
+        book = xlrd.open_workbook( fileName.encode('cp1251'), formatting_info=True)                          # xls
+    return book
+
+
+
+def sheetByName( fileName
+                ,sheetName):
+    typeX = fileName[fileName.find('.')+1 :]
+    try:
+        if typeX.lower() == 'xlsx':
+            book = openpyxl.load_workbook(filename = fileName, read_only=False, keep_vba=False, data_only=False) # xlsx
+            sheet = book[sheetName]                                                                              # xlsx 
+        else:
+            book = xlrd.open_workbook( fileName.encode('cp1251'), formatting_info=True)                          # xls
+            sheet = book.sheet_by_name(sheetName)
+    except Exception as e:
+        print("<%s> <%s> <%S> <<%s>>",fileName, typeX, sheetName,e)
+        sheet = False
+    return sheet
+
+    #sheet = book.worksheets[0]                                                                              # xlsx
+    #sheet = book.sheets()[0]                                                                                # xls
+
+def getCellXlsx(  row       # номер строки
+                , col       # номер колонки 
+                , isDigit   # Признак, числовое ли значение нужно из этого поля
+                , sheet     #  лист XLSX
+                ):
+    '''
+    Функция возвращает значение xls-ячейки в виде строки.    
+    Для цифровых ячеек делается предварительное преобразование 
+    в число (пустые и нечисловые значения преобразуются в "0")
+    '''
+    ccc = sheet.cell(row=row, column=col)
+    cellType  = ccc.data_type
+    cellValue = ccc.value
+    if (isDigit == 'Y') : 
+        if (cellValue == None) : 
+            ss = '0'
+        elif (cellType in ('n')) :                  # numeric
+            if int(cellValue) == cellValue:
+                ss = str(int(cellValue))
+            else :
+                ss = str(cellValue)
+        else :
+#           ss = '0'
+            try:
+                ss = str(float(cellValue.replace(',','.')))
+            except ValueError as e:
+                ss='0'
+    else :
+        if (cellValue == None) : 
+            ss = ''
+        elif (cellType in ('n')) :                    # numeric
+            if int(cellValue) == cellValue:
+                ss = str(int(cellValue))
+            else :
+                ss = str(cellValue)
+        else :
+            ss = str(cellValue)
+    return ss
+
+
+
+def getCell(  row       # номер строки
+            , col       # номер колонки 
+            , isDigit   # Признак, числовое ли значение нужно из этого поля
+            , sheet     #  лист XLS
+            ):
+    '''
+    Функция возвращает значение xls-ячейки в виде строки.    
+    Для цифровых ячеек делается предварительное преобразование 
+    в число (пустые и нечисловые значения преобразуются в "0")
+    '''
+    ccc = sheet.cell(row, col)
+    cellType  = ccc.ctype
+    cellValue = ccc.value
+    if (isDigit == 'Y') : 
+        if (cellValue == '') : 
+            ss = '0'
+        elif (cellType in (2,3)) :                  # numeric
+            if int(cellValue) == cellValue:
+                ss = str(int(cellValue))
+            else :
+                ss = str(cellValue)
+        else :
+            if cellValue == 'по запросу':
+                ss = '0.1'
+            else:
+                ss = '0'
+    else :
+        if (cellType in (2,3)) :                    # numeric
+            if int(cellValue) == cellValue:
+                ss = str(int(cellValue))
+            else :
+                ss = str(cellValue)
+        else :
+            ss = str(cellValue)
+    return ss
+
 
 
 def subInParentheses( sourceString):
-    re_parentheses = re.compile('^.*\(([^)]*)\).*$',  re.IGNORECASE )
+    re_parentheses = re.compile('^.*\(([^)]*)\).*$', re.LOCALE | re.IGNORECASE )
     is_parentheses = re_parentheses.match(sourceString)
     if is_parentheses:                        # Файл соответствует шаблону имени
         key = is_parentheses.group(1)         # выделяю ключ из имени файла
@@ -12,6 +152,27 @@ def subInParentheses( sourceString):
         key = '' 
     return key
 
+
+
+def currencyTypeXlsx(row, col, sheet):
+    '''
+    Функция анализирует "формат ячейки" таблицы excel, является ли он "денежным"
+    и какая валюта указана в этом формате.
+    Распознаются не все валюты и способы их описания.
+    '''
+    c = sheet.cell(row=row, column=col)
+    fmt_str = c.number_format
+    if '\u20bd' in fmt_str:
+        val = 'RUB'
+    elif '\xa3' in fmt_str:
+        val = 'GBP'
+    elif chr(8364) in fmt_str:
+        val = 'EUR'
+    elif (fmt_str.find('USD')>=0) or (fmt_str.find('[$$')>=0) :
+        val = 'USD'
+    else:
+        val = ''
+    return val
 
 
 
@@ -37,6 +198,20 @@ def currencyType(sheet, rowx, colx):
         val = ''
     return val
 
+'''
+
+[$$-409]#,##0.0
+[$$-409]#,##0.0
+[$$-409]#,##0.0
+[$$-409]#,##0.0
+[$$-409]#,##0.0
+#,##0.0"р."
+#,##0.0"р."
+#,##0.0"р."
+#,##0.0"р."
+#,##0.0"р."
+#
+'''
 
 
 
@@ -48,41 +223,6 @@ def dump_cell(sheet, rowx, colx):
     print( rowx, colx, repr(c.value), c.ctype, fmt_obj.type, ccc, chr(ccc) )
     #print( repr(fmt_obj.format_str))
     
-
-
-def getCell(  row       # номер строки
-            , col       # номер колонки 
-            , isDigit   # Признак, числовое ли значение нужно из этого поля
-            , sheet     #  лист XLS
-            ):
-    '''
-    Функция возвращает значение xls-ячейки в виде строки.    
-    Для цифровых ячеек делается предварительное преобразование 
-    в число (пустые и нечисловые значения преобразуются в "0")
-    '''
-    ccc = sheet.cell(row, col)
-    cellType  = ccc.ctype
-    cellValue = ccc.value
-    if (isDigit == 'Y') : 
-        if (cellValue == '') : 
-            ss = '0'
-        elif (cellType in (2,3)) :                  # numeric
-            if int(cellValue) == cellValue:
-                ss = str(int(cellValue))
-            else :
-                ss = str(cellValue)
-        else :
-            ss = '0'
-    else :
-        if (cellType in (2,3)) :                    # numeric
-            if int(cellValue) == cellValue:
-                ss = str(int(cellValue))
-            else :
-                ss = str(cellValue)
-        else :
-            ss = str(cellValue)
-    return ss
-
 
 
 def quoted(sss):
